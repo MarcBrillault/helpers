ff54c5135080f55baa0c8a79de389bdfca0a1292
Marc Brillault <marc@brillault.fr>
2017-03-23 14:19:47 +0100

    getCliArguments - die if $allowedArgs and no arg

--- src/cliHelpers.php
+++ src/cliHelpers.php
@@ -10,23 +10,22 @@ function getCliArguments(array $allowedArgs = [])
 {
     $return      = [];
     $regArgument = '/^-[\w]+$/';
-
     global $argv;
     unset($argv[0]);
-
     try {
-        $previousArg = null;
+        if (!empty($allowedArgs) && empty($argv)) {
+            throw new Exception('There should be at least one argument');
+        }
 
+        $previousArg = null;
         foreach ($argv as $arg) {
             if (preg_match($regArgument, $arg)) {
                 // New argument detected
                 $currentArg = trim($arg, '-');
-
                 if (!empty($allowedArgs) && !in_array($currentArg, $allowedArgs)) {
                     // Error if the given argument is not whitelisted
                     throw new Exception(sprintf('Argument not allowed : %s', $currentArg));
                 }
-
                 // New argument's default value is always true
                 $return[$currentArg] = true;
                 $previousArg         = $currentArg;
@@ -42,10 +41,9 @@ function getCliArguments(array $allowedArgs = [])
         }
     } catch (Exception $e) {
         $errorMessage = $e->getMessage() . PHP_EOL;
-
         if (!empty($allowedArgs)) {
             // If $allowedArgs is set, an error triggers the manual
-            $errorMessage .= 'Allowed params are :' . PHP_EOL;
+            $errorMessage .= 'Allowed arguments are :' . PHP_EOL;
             foreach ($allowedArgs as $key => $value) {
                 $errorMessage .= sprintf("\t-%s" . PHP_EOL, $value);
             }
